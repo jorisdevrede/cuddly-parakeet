9a73318443092d0832988125bed7a790515a4390
Joris de Vrede <jorisdevrede@gmail.com>
2016-02-28 20:34:23 +0000

    PEP corrections

--- samples/hadoop-streaming/chunk.py
+++ samples/hadoop-streaming/chunk.py
@@ -2,49 +2,54 @@
 
 import cStringIO
 
+
 class Processor:
     """Basic Processor implementation"""
-    
+
+    def __init__(self):
+        pass
+
     def do(self, element):
         print element
 
+
 class Chunk:
     """Cuts XML files into separate chunks"""
-    
+
     processor = None
-    
+
     def __init__(self):
         self.processor = Processor()
-    
+
     def __init__(self, processor):
         self.processor = processor
-    
+
     def cut(self, stream, start, end):
         """Cuts an inputstream into chunks and feeds it to Processor.do()
         
         """
-        buffer = None
+        cbuffer = None
         intag = False
-        
+
         for line in stream:
             line = line.strip()
             if start in line:
                 print 'start', line
                 intag = True
-                buffer = cStringIO.StringIO()
-                buffer.write(line)
+                cbuffer = cStringIO.StringIO()
+                cbuffer.write(line)
             elif end in line:
                 print 'end', line
                 intag = False
-                buffer.write(line)
-                chunk = buffer.getvalue()
-                buffer.close()
-                buffer = None
-                
+                cbuffer.write(line)
+                chunk = cbuffer.getvalue()
+                cbuffer.close()
+                cbuffer = None
+
                 # do processing                
                 self.processor.do(chunk)
 
             else:
                 print 'else', line
                 if intag:
-                    buffer.write(line)
+                    cbuffer.write(line)
